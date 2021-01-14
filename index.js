d88e001c9fb1ca15d9c03b8129132ca470294be4
Temirlan <bamblebee988@gmail.com>
2021-01-14 09:48:21 +0600

    Step2: add format option

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import program from 'commander';
-import genDiff from './src/genDiff';
+import genDiff from './src/genDiff.js';
 
 program
   .version('0.0.2')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
+
+console.log(`format: ${program.format}`);
 
 export default genDiff;
