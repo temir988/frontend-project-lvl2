63f39e6226b76415d57c41329e7d2a5fdc0247c9
Temirlan Smagulov <Temirlan.Smagulov@infowatch.com>
2021-06-03 10:17:20 +0600

    add test for yaml files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,15 +6,24 @@ import genDiff from '../src/genDiff';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+let result;
+const fixturesPath = `${__dirname}/../__fixtures__`;
+
+beforeAll(() => {
+  result = fs.readFileSync(`${fixturesPath}/result1.txt`, 'utf-8');
+});
+
 describe('Testing gendiff', () => {
   test('Basic 1 lvl json', () => {
-    const result = fs.readFileSync(
-      `${__dirname}/../__fixtures__/result1.txt`,
-      'utf-8'
-    );
+    const filepath1 = `${fixturesPath}/file1.json`;
+    const filepath2 = `${fixturesPath}/file2.json`;
 
-    const filepath1 = `${__dirname}/../__fixtures__/file1.json`;
-    const filepath2 = `${__dirname}/../__fixtures__/file2.json`;
+    expect(genDiff(filepath1, filepath2)).toBe(result);
+  });
+
+  test('Basic 1 level yaml', () => {
+    const filepath1 = `${fixturesPath}/file1.yaml`;
+    const filepath2 = `${fixturesPath}/file2.yaml`;
 
     expect(genDiff(filepath1, filepath2)).toBe(result);
   });
