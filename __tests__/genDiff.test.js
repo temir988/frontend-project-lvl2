3985aef233ca1c8207f3d9398ab2da848db27ea5
Temirlan <bamblebee988@gmail.com>
2021-06-05 11:37:05 +0600

    refactor paths in test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,29 +1,34 @@
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 import genDiff from '../src/genDiff';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 let result;
-const fixturesPath = `${__dirname}/../__fixtures__`;
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+
+const readFileSync = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 beforeAll(() => {
-  result = fs.readFileSync(`${fixturesPath}/result1.txt`, 'utf-8');
+  result = readFileSync('result1.txt');
 });
 
 describe('Testing gendiff', () => {
   test('Basic 1 level json', () => {
-    const filepath1 = `${fixturesPath}/file1.json`;
-    const filepath2 = `${fixturesPath}/file2.json`;
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
 
     expect(genDiff(filepath1, filepath2)).toBe(result);
   });
 
   test('Basic 1 level yaml', () => {
-    const filepath1 = `${fixturesPath}/file1.yaml`;
-    const filepath2 = `${fixturesPath}/file2.yaml`;
+    const filepath1 = getFixturePath('file1.yaml');
+    const filepath2 = getFixturePath('file2.yaml');
 
     expect(genDiff(filepath1, filepath2)).toBe(result);
   });
